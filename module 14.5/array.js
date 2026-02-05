c685f90b0b3a76944e88502dc8a28113b24c626e
nawshadpro01-gif <nawshadprofessional01@gmail.com>
2026-02-05 10:07:17 +0600

    last push of this milestone

--- module 14.5/array.js
+++ module 14.5/array.js
@@ -1,3 +1,14 @@
-const number = [1, 2, 3, 4]
-// console.log(number);
-console.log(number.length);
+// const number = [1, 2, 3, 4]
+// // console.log(number);
+// console.log(number.length);
+
+
+
+// const numbers = [ 99, 88, 77, 100]
+// console.log(numbers[3])
+
+// set or update element value by index 
+
+const numbers = [ 99, 88, 77, 100]
+numbers.push(44)
+console.log(numbers);
