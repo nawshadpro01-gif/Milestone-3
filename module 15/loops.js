7b0c9126b723bf5690c0b4eaaab1773b79eeb39c
nawshadpro01-gif <nawshadprofessional01@gmail.com>
2026-02-06 03:46:39 +0600

    fisrt commit of the day

---
+++ module 15/loops.js
@@ -0,0 +1,8 @@
+const numbers = [99, 88, 77, 100, 200, 400, 55]
+console.log(numbers)
+// console.log(JSON.stringify(numbers))
+// console.log(numbers.join(", "))
+
+for( const i of numbers){
+    console.log(i)
+}
